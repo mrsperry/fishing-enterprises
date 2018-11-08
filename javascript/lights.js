2a81e41c1073ec8ac1f446e121fc45187957194a
Josh <mrjoshuasperry@gmail.com>
2018-11-08 03:25:33 -0500

    implement theme switching

--- javascript/lights.js
+++ javascript/lights.js
@@ -5,6 +5,8 @@ var lights = {
         let name = this.lights ? "dark" : "light";
         $("#page_style")
             .attr("href", "css/" + name + ".css");
+        $("#lights")
+        	.text("lights " + (this.lights ? "on" : "off"));
 
         this.lights = !this.lights;
     }
