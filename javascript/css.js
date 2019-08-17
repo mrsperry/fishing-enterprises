327ece731bae8acd82bec595d72fb5adc74ab848
Josh <mrjoshuasperry@gmail.com>
2019-08-17 17:03:57 -0400

    Update main files and add a CSS swapper

---
+++ javascript/css.js
@@ -0,0 +1,20 @@
+class css {
+    static parent = $("head");
+
+    static load(hrefs) {
+        // Get all CSS links
+        for (let link of css.parent.children(".temp-css-link")) {
+            link.remove();
+        }
+
+        // Load all new CSS
+        for (let href of hrefs) {
+            $("<link>")
+                .attr("rel", "stylesheet")
+                .attr("type", "text/css")
+                .attr("href", "css/" + href + ".css")
+                .addClass("temp-css-link")
+                .appendTo(css.parent);
+        }
+    }
+}
---
+++ javascript/main.js
@@ -0,0 +1,6 @@
+class main {
+    static initialize() {
+        // Load the main menu
+        main_menu.initialize();
+    }
+}
