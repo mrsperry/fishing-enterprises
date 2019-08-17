39ff24f22a0783ef5a5f229d7309c26446088bcf
Josh <mrjoshuasperry@gmail.com>
2019-08-17 17:02:54 -0400

    Add a main menu

---
+++ javascript/menus/main_menu.js
@@ -0,0 +1,62 @@
+class main_menu {
+    static initialize() {
+        // Load CSS
+        css.load(["main_menu"]);
+
+        // Create a parent element
+        let parent = $("<div>")
+            .attr("id", "main-menu-content")
+            .appendTo($("#content"));
+
+        // Load the banner image
+        $("<img>")
+            .attr("src", "images/banner.png")
+            .appendTo(parent);
+
+        // Create the header text
+        let header = $("<h1>")
+            .text("Fishing Enterprises")
+            .appendTo(parent);
+        $("<div>")
+            .addClass("line-break")
+            .appendTo(header);
+
+        // Create the menu buttons
+        new button({
+            parent: parent,
+            text: "Start Game",
+            on_click: main_menu.start_game
+        });
+        new button({
+            parent: parent,
+            text: "Load Game",
+            on_click: main_menu.load_game
+        });
+        new button({
+            parent: parent,
+            text: "Options",
+            on_click: main_menu.show_options
+        });
+        new button({
+            parent: parent,
+            text: "About",
+            on_click: main_menu.show_about
+        });
+    }
+
+    static start_game() {
+
+    }
+
+    static load_game() {
+
+    }
+
+    static show_options() {
+
+    }
+
+    static show_about() {
+
+    }
+}
