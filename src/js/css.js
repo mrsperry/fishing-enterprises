5144e6828241603022f03ac42bc645e4613e34d8
Josh <mrjoshuasperry@gmail.com>
2019-08-18 23:13:43 -0400

    Split css methods to allow for more control

--- src/js/css.js
+++ src/js/css.js
@@ -2,11 +2,6 @@ class css {
     static parent = $("head");
 
     static load(hrefs) {
-        // Get all CSS links
-        for (const link of css.parent.children(".temp-css-link")) {
-            link.remove();
-        }
-
         // Load all new CSS
         for (const href of hrefs) {
             $("<link>")
@@ -16,5 +11,17 @@ class css {
                 .addClass("temp-css-link")
                 .appendTo(css.parent);
         }
+    }
+
+    static remove() {
+        // Get all CSS links
+        for (const link of css.parent.children(".temp-css-link")) {
+            link.remove();
+        }
+    }
+
+    static load_and_remove(hrefs) {
+        css.remove();
+        css.load(hrefs);
     }
 }
