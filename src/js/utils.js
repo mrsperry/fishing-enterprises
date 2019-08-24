f41138bc5bd71183541e815dff4531ced62125c8
Josh <mrjoshuasperry@gmail.com>
2019-08-24 11:43:38 -0400

    Add a stringify method to add commas to numbers

--- src/js/utils.js
+++ src/js/utils.js
@@ -8,4 +8,9 @@ class utils {
     static random(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
+
+    // Inserts a comma every three numbers (1000 -> 1,000)
+    static stringify(number) {
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
 }
