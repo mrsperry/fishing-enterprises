9bab8af7b0896b85bfb81a06eacef44d3c806d0a
Josh <mrjoshuasperry@gmail.com>
2019-08-26 14:30:37 -0400

    Add misc data

---
+++ src/js/data/misc_data.js
@@ -0,0 +1,30 @@
+class misc_data {
+    static initialize() {
+        for (const index in misc_data.data) {
+            const section = misc_data.data[index];
+
+            // Set default values
+            section.count = 0;
+
+            if (index != "fish_meta") {
+                section.total = 0;
+            }
+        }
+    }
+
+    static get(index) {
+        return misc_data.data[index];
+    }
+
+    static data = {
+        money: {
+            internal: "money",
+            display: "Money",
+            message: "Money often costs too much, but I doubt the fish care."
+        },
+        fish_meta: {
+            internal: "fish_meta",
+            display: "Total fish caught"
+        }
+    }
+}
