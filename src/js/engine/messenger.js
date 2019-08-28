53ee0074bc8aed7620d86e5dde51b29145d53d60
Josh <mrjoshuasperry@gmail.com>
2019-08-28 06:25:11 -0400

    Fix messages not appearing in the correct order

--- src/js/engine/messenger.js
+++ src/js/engine/messenger.js
@@ -20,15 +20,16 @@ class messenger {
     static write(string) {
         const messages = messenger.messages;
 
-        // Remove item from the start of the array
-        if (messages.length > 5) {
-            messages.shift();
-        }
-        // Add item to the end of the array
-        messages.push(string);
+        // Add item to the start of the array
+        messages.unshift(string);
 
-        for (let index = messages.length; index > 0; index--) {
-            $("#history-message-" + index)
+        // Remove item from the end of the array
+        if (messages.length > 5) {
+            messages.pop();
+        }
+
+        for (let index = 0; index < messages.length; index++) {
+            $("#history-message-" + (index + 1))
                 .text(messages[index]);
         }
     }
