cc817e729cb0a704d8bad6fdb79c76243a71f583
Josh <mrjoshuasperry@gmail.com>
2019-09-12 08:58:52 -0400

    Add tackle clickables to the shop

--- src/js/areas/shop.js
+++ src/js/areas/shop.js
@@ -33,20 +33,22 @@ class shop {
                     .text(art_data.get("shop", "license-decor"))
                     .appendTo(license);
 
+                const data = fishing_data.get_data();
+
                 // Create bait clickables
-                const parent = $("<div>")
+                const bait_parent = $("<div>")
                     .attr("id", "bait-item-holder")
                     .addClass("art")
                     .appendTo(art);
 
-                // Loop through all bait
-                const data = fishing_data.get_data();
                 for (const internal in data.bait) {
                     const bait = data.bait[internal];
 
+                    // Create the bait cans
                     const bait_art = $("<div>")
                         .addClass("shop-item flex flex-justify-center")
                         .text(art_data.get("shop", "can"))
+                        // Tooltip show/hide
                         .hover(() => {
                             $("#" + bait.internal + "-tooltip")
                                 .stop()
@@ -56,18 +58,59 @@ class shop {
                                 .stop()
                                 .fadeOut();
                         })
-                        .appendTo(parent);
+                        .appendTo(bait_parent);
+                    // Overlay the bait art
                     $("<div>")
                         .attr("id", bait.internal + "-decor")
-                        .addClass("shop-item-decor")
+                        .addClass("bait-decor")
                         .html(art_data.get("shop", bait.internal + "-decor"))
                         .appendTo(bait_art);
+                    // Create the tooltip element
                     $("<div>")
                         .attr("id", bait.internal + "-tooltip")
-                        .addClass("tooltip")
+                        .addClass("bait-tooltip")
                         .text(bait.display + " ($" + bait.price + ")")
                         .hide()
                         .appendTo(bait_art);
+                }
+
+                // Create tackle clickables
+                const tackle_parent = $("<div>")
+                    .attr("id", "tackle-item-holder")
+                    .addClass("art")
+                    .appendTo(art);
+
+                for (const internal in data.tackle) {
+                    const tackle = data.tackle[internal];
+
+                    // Create the tackle hooks
+                    const tackle_art = $("<div>")
+                        .addClass("shop-item flex flex-justify-center tackle-hook")
+                        .text(art_data.get("shop", "hook"))
+                        .hover(() => {
+                            $("#" + tackle.internal + "-tooltip")
+                            .stop()
+                            .fadeIn();
+                        }, () => {
+                            $("#" + tackle.internal + "-tooltip")
+                            .stop()
+                            .fadeOut();
+                        })
+                        .appendTo(tackle_parent);
+                    
+                    // Overlay the tackle art
+                    $("<div>")
+                        .attr("id", tackle.internal + "-decor")
+                        .addClass("tackle-decor")
+                        .html(art_data.get("shop", tackle.internal + "-decor"))
+                        .appendTo(tackle_art);
+                    // Create the tooltip element
+                    $("<div>")
+                        .attr("id", tackle.internal + "-tooltip")
+                        .addClass("tackle-tooltip")
+                        .text(tackle.display + " ($" + tackle.price + ")")
+                        .hide()
+                        .appendTo(tackle_art);
                 }
             });
     }
