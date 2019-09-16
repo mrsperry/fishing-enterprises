9d7ddffd6adb6f9ed66e0b467572c8560cc7b638
Josh <mrjoshuasperry@gmail.com>
2019-09-16 02:01:30 -0400

    Add generic and specific item buying

--- src/js/areas/shop.js
+++ src/js/areas/shop.js
@@ -88,6 +88,7 @@ class shop {
 
                 const holder = $("<div>")
                     .addClass("shop-item no-select flex flex-justify-center")
+                    .click(() => shop.buy_consumable(item))
                     // Tooltip show/hide
                     .hover(() => {
                         $("#" + item.internal + "-tooltip")
@@ -181,5 +182,80 @@ class shop {
         // Remove door tooltip
         $("#door-tooltip")
             .remove();
+    }
+
+    // Generic buy checks
+    static buy(item) {
+        const money = misc_data.get("money");
+
+        // Check if the player has enough money to buy this item
+        if (money.count < item.price) {
+            messenger.write("You don't have enough money!");
+            return false;
+        }
+
+        // Check if the player can hold this item
+        if (item.count == item.max) {
+            messenger.write("You can't hold any more!");
+            return false;
+        }
+
+        // Check if the item's message should be displayed
+        if (item.show_message != true) {
+            messenger.write(item.display + ": " + item.message);
+            item.show_message = true;
+        }
+
+        // Mark this item as purchased for saving
+        item.purchased = true;
+
+        // Subtract the cost of the item
+        money.count -= item.price;
+
+        // Display the new money count
+        $("#money-count")
+            .text(money.count);
+
+        return true;
+    }
+
+    // Handles buying of bait or tackle
+    static buy_consumable(item) {
+        // Check base conditions
+        if (!shop.buy(item)) {
+            return false;
+        }
+
+        // Declare all relevant elements
+        const count = $("#" + item.internal + "-count");
+        const max = $("#" + item.internal + "-max");
+        const holder = count.parent();
+        const parent = holder.parent();
+
+        // Check if the holder's parent is visible (applicable for tackle or if the lake was skipped)
+        if (parent.css("visibility") == "hidden") {
+            parent.css("visibility", "")
+                .hide()
+                .fadeIn();
+        }
+
+        // Check if the counter's parent is hidden
+        if (holder.is(":hidden")) {
+            holder.fadeIn();
+        }
+
+        // Increment and display the new count
+        count.text(++item.count);
+
+        // Check if the item's max should be shown
+        if (item.count == item.max) {
+            if (max.is(":hidden")) {
+                max.show();
+                item.show_max = true;
+            }
+
+            // Fade out the item
+            count.css("opacity", 0.5);
+        }
     }
 }
