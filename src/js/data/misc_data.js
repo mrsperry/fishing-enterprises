b9d2c03580f716cd20374d59744f68f6cbd37550
Josh <mrjoshuasperry@gmail.com>
2019-09-23 05:36:53 -0400

    Add a money difference display & method for updating money

--- src/js/data/misc_data.js
+++ src/js/data/misc_data.js
@@ -16,11 +16,34 @@ class misc_data {
         return misc_data.data[index];
     }
 
+    static update_money(amount) {
+        const money = misc_data.get("money");
+        money.count += amount;
+        money.difference += amount;
+
+        // Set the money display
+        $("#money-count")
+            .text(utils.stringify(money.count));
+
+        // Set the difference display
+        $("#money-difference")
+            .text(" (" + (money.difference >= 0 ? "+" : "-") + utils.stringify(Math.abs(money.difference)) + ")")
+            // When updating, stop any animation and show the display, this creates a refresh look
+            .stop()
+            .show()
+            .css("opacity", 1)
+            .fadeOut(1200, () => {
+                // Reset the difference
+                money.difference = 0;
+            });
+    }
+
     static data = {
         money: {
             internal: "money",
             display: "Money",
-            message: "Money often costs too much, but I doubt the fish care."
+            message: "Money often costs too much, but I doubt the fish care.",
+            difference: 0
         },
         fish_meta: {
             internal: "fish_meta",
--- src/js/main.js
+++ src/js/main.js
@@ -34,11 +34,6 @@ class main {
         window.setTimeout(main.update, 1000);
     }
 
-    static money() {
-        const money = misc_data.get("money");
-        money.count += 50000;
-    }
-
     static update() {
         // Update the time played
         let time = settings.get("time-played");
