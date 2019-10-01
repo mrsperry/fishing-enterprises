4a5ec800d3f837341d6f895bbe4a9ddc1f0350f7
BuildTools <unconfigured@null.spigotmc.org>
2019-10-01 18:37:09 -0400

    Add achievement system

---
+++ src/js/engine/achievements.js
@@ -0,0 +1,46 @@
+class achievements {
+    static award(id) {
+        const data = achievements.achievement_list[id];
+
+        const element = $("<div>")
+            .addClass("achievement flex")
+            .animate({
+                marginTop: 100
+            }, 1000, function() {
+                setTimeout(function() {
+                    element.animate({
+                        marginTop: 0
+                    }, 1000, function() {
+                        element.remove();
+                    });
+                }, 5000);
+            })
+            .appendTo($("body"));
+
+        $("<div>")
+            .addClass("achievement-art pre bold flex flex-centered")
+            .text(art_data.get("achievements", id))
+            .appendTo(element);
+
+        const text = $("<div>")
+            .addClass("achievement-text centered") 
+            .appendTo(element);
+        
+        $("<div>")
+            .addClass("achievement-title bold")
+            .text(data.title)
+            .appendTo(text);
+
+        $("<div>")
+            .addClass("achievement-desc")
+            .text(data.description)
+            .appendTo(text);
+    }
+
+    static achievement_list = {
+        "10-fish": {
+            title: "Beginner's Luck",
+            description: "Catch your first 10 fish."
+        }
+    };
+}
